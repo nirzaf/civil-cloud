ab035bb3205668adb61264f72d6549e29421846a
nirzaf <mfmfazrin1986@gmail.com>
2023-11-12 21:31:35 +0300

    Remove commented code and update footer links
    Removed commented code from 'config.ts' regarding frontmatter schema for clarity and cleanliness. Project Default inspectionProfile has been updated, enabling HtmlUnknownAttribute inspection tool to validate custom attribute "astro-icon". Furthermore, accompanying values and settings have been updated appropriately; this ensures better code quality and conformity to best practices. The footer component has undergone changes which include updating the owner name in the copyright section and adjusting the social media links and icons including a LinkedIn icon. Removal and addition of certain social media icons and links keeps the portfolio current and linked to the most relevant platforms. Updated these fields to maintain consistency and clarity across the website.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
