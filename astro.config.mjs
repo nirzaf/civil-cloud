074eea8e4683104b44efb615552fd3ca71881c48
nirzaf <mfmfazrin1986@gmail.com>
2023-11-15 20:50:15 +0300

    Update site URL in astro.config
    Changed the site URL in the astro.config.mjs from 'https://example.com' to 'https://dotnetevangelist.net'. The change was made to accurately represent the correct website within the configuration file. The correct URL ensures that any generated sitemaps or other data based on this config file will point to the right location.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dotnetevangelist.net',
 	integrations: [mdx(), sitemap()],
 });
