f13f56edc3cf0a29d3eef72e3d8d48672dc4c38d
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2023-11-21 10:33:36 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://dotnetevangelist.net',
+	site: 'https://nirzaf.github.io/civil-cloud',
 	integrations: [mdx(), sitemap()],
 });
