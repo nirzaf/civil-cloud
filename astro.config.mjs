69b8030eadf2c517d3d8949b893755fbecba74d2
nirzaf <mfmfazrin1986@gmail.com>
2023-11-29 19:19:58 +0300

    Update site URL and hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://nirzaf.github.io/civil-cloud/',
+	site: 'https://nirzaf.github.io/',
 	integrations: [mdx(), sitemap()],
 });
